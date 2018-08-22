d88a7a9498594a32ea2ff5667ab3ff9aa2699cf9
iden1109 <iden1109@MercytekiAir.iden1109.synology.me>
2018-08-22 23:18:27 +0800

    RxJS

--- src/app/posts/editor/editor.component.ts
+++ src/app/posts/editor/editor.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, FormArray, FormBuilder, Validators } from '@angular/forms';
+import { fromEvent, Subject } from 'rxjs';
+import { debounceTime, switchMap, takeUntil } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.css']
 })
-export class EditorComponent implements OnInit {
+export class EditorComponent implements OnInit, OnDestroy {
 
   post = new FormGroup({
     title: new FormControl('test title', [Validators.required, Validators.minLength(10)]),
@@ -19,18 +22,40 @@ export class EditorComponent implements OnInit {
   });
 
   newPost: FormGroup;
+  destroy$: Subject;
 
   get tags(): FormArray {
     return this.post.get('tags') as FormArray;
   }
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+              private httpClient: HttpClient) { }
 
   ngOnInit() {
     this.newPost = this.fb.group({
       title: this.fb.control('test title form builder')
     });
     // this.post = this.newPost;
+
+    // RxJS
+    /* fromEvent($('test').click).subscribe(resp => {
+    }); */
+    const writeLog = value => this.httpClient.get('http://yahoo.com/writelog' + value);
+    const valueChangeSubscription = this.post.get('title').valueChanges.pipe(
+      takeUntil(this.destroy$),
+      debounceTime(300),
+      switchMap(value => this.httpClient.get('http://yahoo.com/get' + value)),
+      switchMap(value => this.httpClient.get('http://yahoo.com/log' + value)),
+      switchMap(writeLog)
+    )
+    .subscribe(value => {
+      console.log(value);
+    });
+    valueChangeSubscription.unsubscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
   }
 
   createPost() {
